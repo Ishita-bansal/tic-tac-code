b9d7fb9b6e59e790a5de3ce9da92168db6ff8238
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2024-04-21 23:35:44 +0530

    Add files via upload
    commit

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,65 +1,65 @@
-import React, { useEffect, useState } from "react";
-import "./blog.css";
-import img1 from "./images/game1.jpg";
-import { firestoreapp } from "../firestorefirebase/firestoreconfig";
-import {
-    collection,
-    getFirestore,
-    getDocs,
-  } from "firebase/firestore";
-  const firestore = getFirestore(firestoreapp);
-function Blog() {
-  const [recievedata,setrecievedata] = useState([]);
+// import React, { useEffect, useState } from "react";
+// import "./blog.css";
+// import img1 from "./images/game1.jpg";
+// import { firestoreapp } from "../firestorefirebase/firestoreconfig";
+// import {
+//     collection,
+//     getFirestore,
+//     getDocs,
+//   } from "firebase/firestore";
+
+// function Blog() {
+//   const [recievedata,setrecievedata] = useState([]);
 
 
-  const getdocument = async () =>{
-    try{
-        const collectionRef = collection(firestore,"Tic-tacs-games");
-        const querySnapshot = await getDocs(collectionRef);
-      const data = [];
-      querySnapshot.forEach((doc) => {
-        data.push({ id: doc.id, ...doc.data() });
-      });
-      return data;
-    } catch (error) {
-      console.error("Error fetching documents:", error);
-      return [];
-    }
-  }
-    useEffect(()=>{
-        const fetchData = async () => {
-            try {
-              const data = await getdocument();
-              setrecievedata(data);
-            } catch (error) {
-              console.error("Error fetching documents:", error);
-            }
-          };
-          fetchData();  
-    },[])
-  return (
-    <>
-      <div className="background-container">
-        <h1>Blog</h1>
-      </div>
-      <div className="blog-input-field">
-        {
+//   const getdocument = async () =>{
+//     try{
+//         const collectionRef = collection(firestore,"Tic-tacs-games");
+//         const querySnapshot = await getDocs(collectionRef);
+//       const data = [];
+//       querySnapshot.forEach((doc) => {
+//         data.push({ id: doc.id, ...doc.data() });
+//       });
+//       return data;
+//     } catch (error) {
+//       console.error("Error fetching documents:", error);
+//       return [];
+//     }
+//   }
+//     useEffect(()=>{
+//         const fetchData = async () => {
+//             try {
+//               const data = await getdocument();
+//               setrecievedata(data);
+//             } catch (error) {
+//               console.error("Error fetching documents:", error);
+//             }
+//           };
+//           fetchData();  
+//     },[])
+//   return (
+//     <>
+//       <div className="background-container">
+//         <h1>Blog</h1>
+//       </div>
+//       <div className="blog-input-field">
+//         {
 
         
-    recievedata?.map((detail)=>(
-        <div className="cards">
-        <img src={detail.img} alt="image"/>
-        <h2>{detail.title}</h2>
-        <p>
-          {detail.desc}
-        </p>
+//     recievedata?.map((detail)=>(
+//         <div className="cards">
+//         <img src={detail.img} alt="image"/>
+//         <h2>{detail.title}</h2>
+//         <p>
+//           {detail.desc}
+//         </p>
         
-      </div>
-    ))
-}  
-      </div>
-    </>
-  );
-}
+//       </div>
+//     ))
+// }  
+//       </div>
+//     </>
+//   );
+// }
 
-export default Blog;
+// export default Blog;
